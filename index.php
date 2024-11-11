9927e5ac421c0923a426b7f03d8aadcd0102b86a
ituki yamanaka <yamanakasan@icloud.com>
2024-11-11 15:16:13 +0900

    course:10

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
-echo("数字と文字は")
 $test = 123;
 $test = '123やめます';
 $test_1 = "わ"
-$test_2 = $test . $test_1;
-echo($test)
+$test_2 = $test . $test_1
 
 // データ型の検索
 var_dump($test)
+
+// 変わらない文字・数
+const MAX = 'そうですね';
+
 ?>
