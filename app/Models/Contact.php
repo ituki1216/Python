7d38f92217a989ccf5456c1a35b2b1cff84dd6e0
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-14 17:26:16 +0900

    Contact classがmodelを継承し、databaseとやり取りが可能になりました

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,1 +1,13 @@
+namespace App\Models; // codeを整理してclassの重複を避ける
+
+use Illuminate\Database\Wloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model; //modelを継承する？データベースとやり取りする
+
+class Contact extends Model // extend modelによりContactはEloquentを継承しdatabaseとやり取りが可能になる
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'message'] // $fillableを使用することで簡単にuserから送信された情報を取得することができる []で制御をおこなう？
+}
+
 
