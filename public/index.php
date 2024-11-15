ddf92db49b3a420d37b8723ba1b163375b122d18
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-15 16:55:11 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ここはお問い合わせフォーム</title>
-</head>
+<head>
 <body>
   <h1>お問い合わせ</h1>
 </body>
