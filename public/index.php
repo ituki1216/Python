e998009c8c57481d7f4f2f5b20f498e9ea841b80
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-15 17:14:00 +0900

    まずはhtmlだけ

--- public/index.php
+++ public/index.php
@@ -7,5 +7,17 @@
 </head>
 <body>
   <h1>お問い合わせ</h1>
+  <form action="form.php" method="POST">
+      <lavel for="name">名前:</lavel>>br>
+      <input type="text" id="name" name="name" required><br>
+
+      <lavel for="mail_address">メールアドレス:</lavel>>br>
+      <input type="text" id="mail_address" name="mail_address" required><br>
+
+      <lavel for="message">メッセージ:</lavel>>br>
+      <input type="text" id="message" name="message" required><br>
+
+      <input type="submit" value="送信">
+  </form>
 </body>
 </html>
