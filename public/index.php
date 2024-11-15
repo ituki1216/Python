a04b69b3b47685d1c2479dcdc2be30ab14276fce
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-15 17:27:06 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,20 +9,21 @@
   <h1>お問い合わせ</h1>
 
   <?php 
-  if isset($successMessage)) { 
+  if (isset($successMessage)) { 
     echo $successMessage;
   }elseif (isset($errorMessage)) {
     echo $errorMessage;
   }
+      ?>
     
   <form action="form.php" method="POST">
-      <lavel for="name">名前:</lavel>>br>
+      <label for="name">名前:</lavel>>br>
       <input type="text" id="name" name="name" required><br>
 
-      <lavel for="mail_address">メールアドレス:</lavel>>br>
+      <label for="mail_address">メールアドレス:</lavel>>br>
       <input type="text" id="mail_address" name="mail_address" required><br>
 
-      <lavel for="message">メッセージ:</lavel>>br>
+      <label for="message">メッセージ:</lavel>>br>
       <input type="text" id="message" name="message" required><br>
 
       <input type="submit" value="送信">
