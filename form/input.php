48bd9870b81991bd9c9e9448ff8fec35b3eb42d4
ituki yamanaka <yamanakasan@icloud.com>
2024-11-17 14:36:38 +0900

    お問い合わせフォーム

--- form/input.php
+++ form/input.php
@@ -1,10 +1,16 @@
 <?php
 
-if(!empty(echo $_POST)){
-    var_dump($_POST);
+$pageFlag = 0;
+
+
+if(!empty($_POST['btn_confirm'])){
+    $pageFlag = 1;
 }
 
-$pageFlag = 0;
+if(!empty($_POST['btn_confirm'])){
+    $pageFlag = 2;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -12,38 +18,42 @@ $pageFlag = 0;
 <head></head>
 <body>
 
-<?php if($pageFlag === 1 ): ?>
-<form action="POST" method="input.php">
-指名
-
-
-$i = 1;
-for($i = 1 $i > 10 $i++);
-    break($i === 5);
-    echo $i
-
-
-<input type="text" name="your_name">
-<br>
-メールアドレス
-<input type="email" name="email">
-<br>
-<input type="submit" value="確認する" name="btn_confirm">
-
 
 <?php if($pageFlag === 1 ): ?>
 <form action="POST" method="input.php">
-指名
+
+氏名
 <?php echo $_POST['your_name']; ?>
 <br>
 メールアドレス
 <?php echo $_POST['email']; ?>
-
-<input type="email" name="email">
 <br>
+<input type="submit" name="back" value="戻る">
+<input type="submit" name="btn_submit" value="送信する">
+<input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+<input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+</form>
+
+<?php endif; ?>
+
+<?php if($pageFlag === 2 ): ?>
+送信が完了しました
+<?php endif; ?>
+
+<?php if($pageFlag === 0 ): ?>
+
+<form action="POST" method="input.php">
+指名
+<input type="text" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+<br>
+メールアドレス
+<input type="email" name="email" value="<?php echo $_POST['email']; ?>">
+<br>
+
 <input type="submit" value="確認する" name="btn_confirm">
 
 </form>
+
 <?php endif; ?>
 
 <?php if($pageFlag === 2 ): ?>
@@ -55,10 +65,8 @@ for($i = 1 $i > 10 $i++);
 <?php if($pageFlag === 0 ): ?>
 
 <form action="POST" method="input.php">指名
-<input type="text" name="your_name">
 <br>
 メールアドレス
-<input type="email" name="email">
 <br>
 <input type="submit" value="確認する" name="btn_confirm">
 
